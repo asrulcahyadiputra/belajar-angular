516099cad9dbf1fd2e1070fcae8967e1a5dc32e0
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-01-15 12:04:19 +0800

    create the first component

---
+++ src/app/hello-world.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `
+    <h2>Hello World!</h2>
+    <p>Component Angular Pertama</p>
+  `,
+})
+export class HelloWorldComponent {}
