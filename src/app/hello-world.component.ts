4c49aec0b0db6658bf6e9ad7b9a4eb79ce4380bd
asrulcahyadiputra <asrulcahyadiputra99@gmail.com>
2022-01-15 12:09:35 +0800

    template url

--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-world',
-  template: `
-    <h2>Hello World!</h2>
-    <p>Component Angular Pertama</p>
-  `,
+  templateUrl: './components/helloWorld.component.html',
+  // template: `
+  //   <h2>Hello World!</h2>
+  //   <p>Component Angular Pertama</p>
+  // `,
 })
 export class HelloWorldComponent {}
